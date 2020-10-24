1abda3c36ced1270066f7567820f80b2aeea3ae5
Geva Or <gevaor.tech@gmail.com>
2020-10-24 17:31:02 +0300

    Fixed the requirements file

--- nasa-peek-venv/Scripts/activate.bat
+++ nasa-peek-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mr512\Desktop\Python Course\Projects\nasa-peek\nasa-peek-venv
+set VIRTUAL_ENV=C:\Users\mr512\Desktop\Python Course\Projects\Nasa Peek Website\nasa-peek\nasa-peek-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
